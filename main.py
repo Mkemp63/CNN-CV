e8145669e344203a08d6ec6baf677be417e846a9
Mkemp <M.g.r.kemp@students.uu.nl>
2021-03-22 17:42:45 +0100

    GPU or CPU is now set properly, validation/training loss graph is plotted

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
 
+
+
+#GPU stuff
+gpus = tf.config.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Memory issue
+        tf.config.experimental.set_memory_growth(gpus[0], True)
+        # Restrict TensorFlow to only use the first GPU
+        tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+    except RuntimeError as e:
+        # Visible devices must be set before GPUs have been initialized
+        print(e)
+
+if tf.test.gpu_device_name():
+    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+else:
+    print("No GPU found")
 
 
 fashion_mnist = tf.keras.datasets.fashion_mnist
@@ -16,7 +37,6 @@ img_rows = train_images[0].shape[0]
 img_cols = test_images[0].shape[1]
 
 Input_shape = (img_rows, img_cols, 1)
-
 
 model = models.Sequential()
 
@@ -37,3 +57,12 @@ print(model.summary())
 
 history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
 
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+plt.show()
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+print(test_acc)
