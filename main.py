55c32d1dd54cedd47a6a15ddabb270d27887c2d3
martijndekker98 <m.q.dekker@students.uu.nl>
2021-03-22 15:59:21 +0100

    1
    1

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
+
+
+
 fashion_mnist = tf.keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
