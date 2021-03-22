9022372441247ea318ebcd28c4714fd3ead03feb
Mkemp <M.g.r.kemp@students.uu.nl>
2021-03-22 15:55:12 +0100

    Baseline model

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-#initial commit
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+train_images = train_images.reshape((train_images.shape[0], 28, 28, 1))
+test_images = test_images.reshape((test_images.shape[0], 28, 28, 1))
+
+img_rows = train_images[0].shape[0]
+img_cols = test_images[0].shape[1]
+
+Input_shape = (img_rows, img_cols, 1)
+
+
+model = models.Sequential()
+
+model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=Input_shape))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(32, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(10))
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+print(model.summary())
+
+history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
+
