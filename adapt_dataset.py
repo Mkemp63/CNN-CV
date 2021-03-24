0b14ee81a50a497f0e2455d178cd0ec7920fc59b
martijndekker98 <m.q.dekker@students.uu.nl>
2021-03-24 15:02:40 +0100

    f
    f

---
+++ adapt_dataset.py
@@ -0,0 +1,15 @@
+import cv2
+import os
+import config
+
+aantal = 0
+for filename in os.listdir("J:\\Python computer vision\\CNN-CV\\data_fashion\\images"):
+    aantal += 1
+    img = cv2.imread(f"J:\\Python computer vision\\CNN-CV\\data_fashion\\images/"+filename, 0)
+    img = cv2.resize(img, (28, 28))
+    cv2.imwrite(f"J:\\Python computer vision\\CNN-CV\\data_fashion\\converted/{filename}", img)
+    if aantal % 1000 == 0:
+        print(aantal)
+
+print(aantal)
+# cv2.imread(filename, 0)
---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+# Directory information
+ROOT_DIR = os.getcwd()
+MODELS_DIR = os.path.join(ROOT_DIR, 'models/')
+BASELINE_DIR = os.path.join(MODELS_DIR, 'model_baseline')
+
+# Data information
+Image_size = 28
+
+# Divsision
+Validate_perc = 0.2
+
+# Training information
+Max_epochs = 15
+Epochs = 1
+Batch_size = 32
+Use_pretrained = False
